4e81a812cb3ea6a031407aafbb17ff1387605ef5
unknown <nhussaini@luc.edu>
2020-06-21 09:53:15 -0700

    added react icons

--- src/components/Learn.js
+++ src/components/Learn.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import { Helmet } from 'react-helmet';
 
+import { BsCardText } from "react-icons/bs";
+
 export default function Learn() {
     return (
         <>
         <Helmet>
             <title>Learn Page</title>
         </Helmet>
-         <main className="main-page">
-                <h1>Study Mode</h1>
+        <div className="main-page">
+        <h1>Study Mode</h1>
+         <main className="main-content">
                 <div className="controls">
-                    <h1>flashcards</h1>
+                <span className="mdi mdi-card-text"></span>
+                    <h2><span><BsCardText /></span>Flashcards</h2>
                     <p>progress 1/17</p>
+                    <p>
+                        <span className="mdi mdi-lightbulb-on-outline mdi-24px lifeline-icon"></span>
+                    </p>
                     <button>quit X</button>
                 </div>
 
@@ -19,9 +26,14 @@ export default function Learn() {
                     <div className="flashcard">
                         <p>Seletct a lesson to learn</p>
                     </div>
+                    <div className="flashcards-button">
+                        <button>Previous</button>
+                        <button>Next</button>
+                    </div>
 
                  </div>
          </main>
+         </div>
         </>
         
     )
