4402ed7912d1b98374c9596f1eee28dd6fe9f024
unknown <nhussaini@luc.edu>
2020-07-02 09:39:29 -0700

    added the quiz component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 import Home from './components/Home';
 import Learn from './components/Learn';
+import Quiz from './components/Quiz';
 
 
 
@@ -12,6 +13,7 @@ function App() {
     <Router>
       <Route path="/" exact component={Home} />
       <Route path="/learn" component={Learn} />
+      <Route path="/quiz" component={Quiz}/>
     </Router>
   );
 }
---
+++ src/components/Quiz.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+
+class Quiz extends Component{
+
+    render(){
+        return(
+            <div>
+                <p>this is the quiz Component</p>
+            </div>
+        );
+    }
+}
+
+export default Quiz
